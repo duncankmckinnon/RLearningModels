e90790d5fab2d19e4766bcdb2afdd5bd0849cf7d
duncankmckinnon <duncan96@uw.edu>
2017-12-24 13:23:06 -0800

    reformatted for consistent use of arrays in performing convolution operations

--- conv.R
+++ conv.R
@@ -1,10 +1,25 @@
-conv2d <- function(imray, filter, padding = 0, stride = 1)
+#Convolutional NN Tools
+#Duncan McKinnon
+
+#Convolutional NN tools using R Arrays
+
+
+conv2d <- function(imray, filter, padding = 0, stride = 1, sameConv = FALSE)
 {
-  if(padding > 0)
+  if(sameConv)
   {
-    n <- matrix(0, nrow = (dim(imray)[1]+2*padding), ncol = (dim(imray)[2]+2*padding))
-    n[(padding+1):(padding+dim(imray)[1]), (padding+1):(padding+dim(imray)[2])] <- imray
-    imray <- n
+    if(dim(imray)[1] == dim(imray)[2] | length(padding) == 1)
+    {
+      padding <- ceiling((stride * (dim(imray)[1] - 1) + dim(filter)[1] - dim(imray)[1])/2)
+    }
+    else
+    {
+      padding <- c(ceiling((stride * (dim(imray)[1] - 1) + dim(filter)[1] - dim(imray)[1])/2), ceiling((stride * (dim(imray)[2] - 1) + dim(filter)[2] - dim(imray)[2])/2))
+    }
+  }
+  if(padding[1] > 0)
+  {
+    imray <- addPad(imray, padding)
   }
   nf <- dim(filter)
   nx <- dim(imray)
@@ -19,7 +34,6 @@ conv2d <- function(imray, filter, padding = 0, stride = 1)
       }
     }
   }
-  
   suppressWarnings(
     {
       mz <- matrix(z, nrow = floor((nx[1]-nf[1])/stride + 1), ncol = floor((nx[2]-nf[2])/stride+ 1))
@@ -28,37 +42,218 @@ conv2d <- function(imray, filter, padding = 0, stride = 1)
   return(mz)
 }
 
-conv3d <- function(imray, filter, padding = 0, stride = 1)
+
+conv3d <- function(imray, filter, padding = 0, stride = 1, sameConv = FALSE)
 {
   if(stride < 1)
   {
     print("stride length must be > 1")
     stride = 1
   }
-  if(padding > 0)
-  {
-    n <- array(0, dim = c((dim(imray)[1]+2*padding),(dim(imray)[2]+2*padding),dim(imray)[3]))
-    n[(padding+1):(padding+dim(imray)[1]), (padding+1):(padding+dim(imray)[2]),1:dim(imray)[3]] <- imray
-    imray <- n
-  }
+  
   nf <- dim(filter)
   nx <- dim(imray)
-  z <- c()
-  for(i in seq(1, (nx[2]-nf[2] + 1), stride))
+  
+  stopifnot(nx[3] == nf[3])
+
+  if(sameConv)
   {
-    for(j in seq(1, (nx[1]-nf[1] + 1), stride))
+    if(nx[1] == nx[2])
     {
-      if((j+nf[1]-stride < nx[1]) && (i+nf[2]-stride < nx[2])) 
-      {
-        z <- c(z, sum(imray[j:(j+nf[1]-1), i:(i+nf[2]-1), 1:dim(imray)[3]] * filter))
-      }
+      padding <- ceiling((stride * (nx[1] - 1) + nf[1] - nx[1])/2)
+    }
+    else
+    {
+      padding <- c(ceiling((stride * (nx[1] - 1) + nf[1] - nx[1])/2), ceiling((stride * (nx[2] - 1) + nf[2] - nx[2])/2))
     }
   }
   
+  if(padding[1] > 0)
+  {
+    imray <- addPad(imray, padding)
+    nx <- dim(imray)
+  }
+  
+  z <- c()
+  
+  if(length(nf) == 3)
+  {
+    nf <- c(nf, 1)
+  }
+  for(i in 1:nf[4])
+  {
+    for(j in seq(1, (nx[2]-nf[2] + 1), stride))
+    {
+      for(k in seq(1, (nx[1]-nf[1] + 1), stride))
+      {
+        if((k+nf[1]-stride < nx[1]) && (j+nf[2]-stride < nx[2])) 
+        {
+          z <- c(z, sum(imray[k:(k+nf[1]-1), j:(j+nf[2]-1), 1:nx[3]] * filter[1:nf[1], 1:nf[2],1:nx[3], i]))
+        }
+      }
+    }
+  }
   suppressWarnings(
     {
-      mz <- matrix(z, nrow = floor((nx[1]-nf[1])/stride + 1), ncol = floor((nx[2]-nf[2])/stride+ 1))
+      mz <- array(z, dim = c(floor((nx[1]-nf[1])/stride + 1), floor((nx[2]-nf[2])/stride+ 1), nf[4]))
     }
   )
   return(mz)
 }
+
+
+conv <- function(imray, filters, padding = 0, stride = 1)
+{
+  z <- list()
+  df <- dim(filters[[1]])
+  nf <- length(df)
+  ni <- length(dim(imray))
+  n <- length(filters)
+  for(i in 1:n)
+  {
+    if(any(dim(filters[[i]]) != df))
+    {
+      print("error in filter dimensions")
+      return(z)
+    }
+    if(nf == 2)
+    {
+      z[[i]] <- conv2d(imray, filters[[i]], padding, stride)
+    }
+    else if(nf == 3)
+    {
+      z[[i]] <- conv3d(imray, filters[[i]], padding, stride)
+    }
+  }
+  return(array(unlist(z), dim = c(dim(z[[1]])[1], dim(z[[1]])[2], n)))
+}
+
+
+pooling <- function(imray, type = "max", filterSize = c(3,3), stride = 1)
+{
+  if(stride < 1)
+  {
+    print("stride length must be > 1")
+    stride = 1
+  }
+  nx <- dim(imray)
+  nf <- filterSize
+  
+  stopifnot(nx[3] == nf[3])
+  
+  if(length(nf) == length(nx))
+  {
+    filterSize <- c(filterSize, 1)
+    nf <- filterSize
+  }
+  
+  z <- c()
+  for(h in 1:nf[4])
+  {
+    for(i in seq(1, (nx[2]-nf[2] + 1), stride))
+    {
+      for(j in seq(1, (nx[1]-nf[1] + 1), stride))
+      {
+        if((j+nf[1]-stride < nx[1]) && (i+nf[2]-stride < nx[2])) 
+        {
+          m = 0
+          if(length(nx) == 3)
+          {
+            if(type == "max")
+            {
+              m <- max(imray[j:(j+nf[1]-1), i:(i+nf[2]-1), 1:nx[3]])
+            }
+            else if(type == "avg")
+            {
+              m <- mean(imray[j:(j+nf[1]-1), i:(i+nf[2]-1), 1:nx[3]])
+            }
+            z <- c(z, m)
+          }
+          else if(length(nx) == 2)
+          {
+            if(type == "max")
+            {
+              m <- max(imray[j:(j+nf[1]-1), i:(i+nf[2]-1), 1])
+            }
+            else if(type == "avg")
+            {
+              m <- mean(imray[j:(j+nf[1]-1), i:(i+nf[2]-1), 1])
+            }
+            z <- c(z, m)
+          }
+        }
+      }
+    }
+  }
+  suppressWarnings(
+    {
+      if(filterSize[3] == 1)
+      {
+        mz <- matrix(z, nrow = floor((nx[1]-nf[1])/stride + 1), ncol = floor((nx[2]-nf[2])/stride+ 1))
+      }
+      else
+      {
+        mz <- array(z, dim = c(floor((nx[1]-nf[1])/stride + 1), floor((nx[2]-nf[2])/stride+ 1), filterSize[4]))
+      }
+    }
+  )
+  return(mz)
+}
+
+
+maxMask <- function(X)
+{
+  if(is.array(X))
+  {
+    m <- dim(X)
+  }
+  else
+  {
+    m <- length(X)
+  }
+  return(array(as.numeric(X == max(X)), dim = c(m)))
+}
+
+
+addPad <- function(data, pad)
+{
+  if(is.array(data))
+  {
+    dims = dim(data)
+    dlen = length(dims)
+    if(dlen == 2)
+    {
+      if(dims[1] == dims[2] | length(pad) == 1)
+      {
+        padA <- pad
+        padB <- pad
+      }
+      else
+      {
+        padA <- pad[1]
+        padB <- pad[2]
+      }
+      n <- array(0, dim = c(dims[1] + 2 * padA, dims[2] + 2 * padB))
+      n[(padA+1):(padA+dims[1]), (padB+1):(padB+dims[2])] <- data
+      data <- n
+    }
+    else if(dlen == 3)
+    {
+      if(dims[1] == dims[2] | length(pad) == 1)
+      {
+        padA <- pad
+        padB <- pad
+      }
+      else
+      {
+        padA <- pad[1]
+        padB <- pad[2]
+      }
+      n <- array(0, dim = c(dims[1] + 2 * padA, dims[2] + 2 * padB, dims[3]))
+      n[(padA+1):(padA+dims[1]), (padB+1):(padB+dims[2]), 1:dims[3]] <- data
+      data <- n
+    }
+  }
+  return(data)
+}
+
